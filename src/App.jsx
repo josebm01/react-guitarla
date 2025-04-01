8bfda957d91ca96790d040a4931f6b19cd7da1d2
Jose BM <jbaxin@manuelsolis.com>
2025-03-31 23:25:33 -0600

    State update when adding quantities to the cart

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,32 @@ import { Guitar } from "./components/Guitar";
 import { Footer } from "./components/Footer";
 import { db } from "./data/db";
 
-
-
 function App() {
 
   // State
   const [ data, setData ] = useState(db)
+  const [cart, setCart] = useState([])
+
+  const addToCart = ( item ) => {
+
+    const itemExists = cart.findIndex(( guitar ) => guitar.id === item.id )
+    
+    // Actualizando cantidad o nueva guitarra
+    if ( itemExists >= 0) { // existe en el carrito 
+
+      // Para no mutar el state
+      const updatedCart = [ ...cart ]
+      updatedCart[ itemExists ].quantity++
+      setCart(updatedCart)
+
+    } else { // no existe y se agrega la cantida de 1
+      item.quantity = 1
+      setCart([ ...cart, item ])
+      // setCart((prevState) => [ ...prevState, item ])
+    }
+
+  }
+  
 
   return (
     <>
@@ -20,7 +40,12 @@ function App() {
         <div className="row mt-5">
           {
             data.map((guitar) => (
-              <Guitar key={guitar.id} guitar={guitar} />
+              <Guitar 
+                 key={ guitar.id } 
+                guitar={ guitar } 
+                setCart={ setCart }
+                addToCart={ addToCart}
+              />
             ))
           }
         </div>
--- src/components/Guitar.jsx
+++ src/components/Guitar.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-export const Guitar = ({ guitar }) => {
+export const Guitar = ({ guitar, addToCart }) => {
   
-  const { name, image, description, price  } = guitar
+  const { id, name, image, description, price  } = guitar
 
   return (
     <div className="col-md-6 col-lg-4 my-4 row align-items-center">
@@ -19,7 +19,11 @@ export const Guitar = ({ guitar }) => {
           { description }
         </p>
         <p className="fw-black text-primary fs-3">${ price }</p>
-        <button type="button" className="btn btn-dark w-100">
+        <button 
+          type="button" 
+          className="btn btn-dark w-100"
+          onClick={ () => addToCart(guitar) }  
+        >
           Agregar al Carrito
         </button>
       </div>
